78f9af3ccedaf5f81c3f501bdcd209b8cc9e939e
Arkadiusz Migała <arkadiusz.migala@performgroup.com>
2019-08-30 17:35:19 +0200

    Updated readme.md, fixed scripts to use plugin fields

---
+++ README.md
@@ -0,0 +1,24 @@
+# drone-sns-publish
+
+## Drone configuration examples
+
+```yaml
+pipeline:
+  release-notify:
+    image: ferdezo/drone-sns-publish
+    topic_arn: arn:aws:sns:***:***:drone-releases
+    subject: ${DRONE_REPO_NAME} release successful
+    message: Successul released version ${DRONE_TAG} from branch ${DRONE_BRANCH} by ${DRONE_COMMIT_AUTHOR}
+```
+
+```yaml
+pipeline:
+  release-failure-notify:
+    image: ferdezo/drone-sns-publish
+    topic_arn: arn:aws:sns:***:***:drone-releases
+    subject: ${DRONE_REPO_NAME} release failure
+    message: Failed to release version ${DRONE_TAG} from branch ${DRONE_BRANCH} by ${DRONE_COMMIT_AUTHOR}
+  when:
+    event: tag
+    status: failure
+```
--- scripts/aws-sns.sh
+++ scripts/aws-sns.sh
@@ -2,26 +2,26 @@
 
 set -e
 
-if [ -z $TOPIC_ARN ];
+if [ -z $PLUGIN_TOPIC_ARN ];
 then
     echo "Error: please provide topic ARN to publish to"
     exit 1
 fi
 
-if [ -z $MESSAGE ];
+if [ -z $PLUGIN_MESSAGE ];
 then
     echo "Error: please provide publish message"
     exit 1
 fi
 
-if [ -z $SUBJECT ];
+if [ -z $PLUGIN_SUBJECT ];
 then
-    SUBJECT="Drone Message"
+    PLUGIN_SUBJECT="Drone Message"
 fi
 
-echo "Sending message: ${MESSAGE} with subject: ${SUBJECT} to topic: ${TOPIC_ARN}"
+echo "Sending message: ${PLUGIN_MESSAGE} with subject: ${PLUGIN_SUBJECT} to topic: ${PLUGIN_TOPIC_ARN}"
 
 aws sns publish \
---topic-arn ${TOPIC_ARN} \
---message ${MESSAGE} \
---subject ${SUBJECT}
+--topic-arn ${PLUGIN_TOPIC_ARN} \
+--message ${PLUGIN_MESSAGE} \
+--subject ${PLUGIN_SUBJECT}
